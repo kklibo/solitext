d21e36d865b18fad835e7d7d00b835456b37a0b4
kklibo <kklibo4@gmail.com>
2023-02-20 18:32:11 -0800

    cursor display

--- src/tui.rs
+++ src/tui.rs
@@ -5,14 +5,23 @@ use termion::input::TermRead;
 use termion::raw::{IntoRawMode, RawTerminal};
 use termion::{clear, color, cursor};
 
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+enum Selection {
+    Deck,
+    Column { index: u8, card_count: u8 },
+    Pile { index: u8 },
+}
+
 pub struct Ui {
     stdout: RawTerminal<Stdout>,
+    cursor: Selection,
 }
 
 impl Ui {
     pub fn new() -> Self {
         Self {
             stdout: stdout().into_raw_mode().unwrap(),
+            cursor: Selection::Deck,
         }
     }
 
@@ -20,10 +29,34 @@ impl Ui {
         self.display_deck(game_state);
         self.display_columns(game_state);
         self.display_piles(game_state);
+        self.display_selection_cursor();
     }
 
+    const CURSOR_ROW: u16 = 10;
+    fn display_selection_cursor(&mut self) {
+        let col = match self.cursor {
+            Selection::Deck => Self::DECK_INIT_COL,
+            Selection::Column { index, .. } => {
+                Self::COLUMNS_INIT_COL + (index as u16) * Self::COLUMNS_COL_STEP
+            }
+            Selection::Pile { .. } => Self::PILES_INIT_COL,
+        };
+
+        writeln!(
+            self.stdout,
+            "{}{}{}█↑█",
+            cursor::Goto(col, Self::CURSOR_ROW),
+            cursor::Hide,
+            color::Fg(color::Blue),
+        )
+        .unwrap();
+    }
+
+    const COLUMNS_INIT_COL: u16 = 8;
+    const COLUMNS_INIT_ROW: u16 = 2;
+    const COLUMNS_COL_STEP: u16 = 5;
     fn display_columns(&mut self, game_state: &GameState) {
-        let (init_col, init_row) = (8u16, 2u16);
+        let (init_col, init_row) = (Self::COLUMNS_INIT_COL, Self::COLUMNS_INIT_ROW);
         let mut col = init_col;
         for column in &game_state.columns {
             let mut row = init_row;
@@ -40,12 +73,14 @@ impl Ui {
 
                 row += 1;
             }
-            col += 5;
+            col += Self::COLUMNS_COL_STEP;
         }
     }
 
+    const PILES_INIT_COL: u16 = 48;
+    const PILES_INIT_ROW: u16 = 2;
     fn display_piles(&mut self, game_state: &GameState) {
-        let (init_col, init_row) = (48u16, 2u16);
+        let (init_col, init_row) = (Self::PILES_INIT_COL, Self::PILES_INIT_ROW);
         let mut row = init_row;
         for pile in &game_state.card_piles {
             let top = if let Some(card) = pile.0.last() {
@@ -68,8 +103,10 @@ impl Ui {
         }
     }
 
+    const DECK_INIT_COL: u16 = 2;
+    const DECK_INIT_ROW: u16 = 2;
     fn display_deck(&mut self, game_state: &GameState) {
-        let (init_col, init_row) = (2u16, 2u16);
+        let (init_col, init_row) = (Self::DECK_INIT_COL, Self::DECK_INIT_ROW);
 
         let top = if let Some(card) = game_state.deck.last() {
             card.to_string()
