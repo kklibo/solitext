363f5f9aa9338dca71bd3f499c1f4ada16933a29
kklibo <kklibo4@gmail.com>
2023-03-07 11:20:58 -0800

    move code to draw::card_column submodule

--- src/draw.rs
+++ src/draw.rs
@@ -1,3 +1,5 @@
+mod card_column;
+
 use crate::cards::{Card, Suit};
 use crate::game_state::{CardState, GameState};
 use crate::selection::Selection;
@@ -13,16 +15,6 @@ pub struct Draw {
     pub context_help_message: String,
     pub debug_message: String,
     pub debug_mode: bool,
-}
-
-enum CardColumnScroll {
-    AtMaxRow,
-    AtMinRow,
-}
-
-struct ScrolledColumn {
-    visible_cards: Vec<(Card, CardState)>,
-    at_edge: Option<CardColumnScroll>,
 }
 
 impl Draw {
@@ -118,37 +110,6 @@ impl Draw {
         self.draw_text(col - 2, row, "▸");
     }
 
-    fn draw_card_column_selection_cursor(
-        &mut self,
-        game_state: &GameState,
-        col: usize,
-        index: usize,
-        card_count: usize,
-    ) {
-        let length = game_state.columns[index].0.len();
-        let (scroll, _) = Self::scrolled_column_offset(length, card_count).unwrap_or((0, None));
-
-        let upper = Self::COLUMNS_INIT_ROW
-            + Self::COLUMNS_ROW_STEP
-                * length
-                    .checked_sub(scroll)
-                    .expect("column scroll should not exceed total cards");
-        let lower = upper
-            .checked_sub(card_count)
-            .expect("should not select nonexistent cards");
-
-        // Don't draw past the end of the column
-        let upper = std::cmp::min(
-            upper,
-            Self::COLUMNS_INIT_ROW + Self::COLUMNS_ROW_STEP * Self::COLUMN_MAX_VISIBLE_CARDS,
-        );
-
-        for row in lower..upper {
-            self.draw_text(col - 1, row, "[");
-            self.draw_text(col + 3, row, "]");
-        }
-    }
-
     fn draw_pile_selection_cursor(&mut self, col: usize, index: usize) {
         let row = Self::PILES_INIT_ROW + Self::PILES_ROW_STEP * index;
         self.draw_text(col - 1, row, "[");
@@ -182,97 +143,6 @@ impl Draw {
         };
 
         self.draw_text(col, row, text.as_str());
-    }
-
-    const COLUMN_MAX_VISIBLE_CARDS: usize = 7;
-    /// Scrolled offset in card column + position info, or None if not scrolled
-    fn scrolled_column_offset(
-        cards: usize,
-        selected: usize,
-    ) -> Option<(usize, Option<CardColumnScroll>)> {
-        use std::cmp::min;
-        if cards <= Self::COLUMN_MAX_VISIBLE_CARDS {
-            return None;
-        }
-
-        let range = cards - Self::COLUMN_MAX_VISIBLE_CARDS;
-        let a = cards - min(selected, cards);
-        let offset = min(a, range);
-
-        let position = match offset {
-            0 => Some(CardColumnScroll::AtMaxRow),
-            x if x == range => Some(CardColumnScroll::AtMinRow),
-            _ => None,
-        };
-
-        Some((offset, position))
-    }
-
-    /// A scrolled card column's visible cards, or None if not scrolled
-    fn scrolled_column(cards: &Vec<(Card, CardState)>, selected: usize) -> Option<ScrolledColumn> {
-        Self::scrolled_column_offset(cards.len(), selected).map(|(offset, at_edge)| {
-            ScrolledColumn {
-                visible_cards: cards[offset..offset + Self::COLUMN_MAX_VISIBLE_CARDS].into(),
-                at_edge,
-            }
-        })
-    }
-
-    /// A column's active selection count; 0 if not selected.
-    fn selection_count(&mut self, column_index: usize) -> usize {
-        if let Selection::Column { index, card_count } = self.cursor {
-            if column_index == index {
-                return card_count;
-            }
-        }
-        if let Some(Selection::Column { index, card_count }) = self.selected {
-            if column_index == index {
-                return card_count;
-            }
-        }
-        0
-    }
-
-    const COLUMNS_INIT_COL: usize = 8;
-    const COLUMNS_INIT_ROW: usize = 2;
-    const COLUMNS_COL_STEP: usize = 5;
-    const COLUMNS_ROW_STEP: usize = 1;
-    fn display_columns(&mut self, game_state: &GameState) {
-        use termion::color::*;
-        let (init_col, init_row) = (Self::COLUMNS_INIT_COL, Self::COLUMNS_INIT_ROW);
-        let mut col = init_col;
-        for (index, column) in game_state.columns.iter().enumerate() {
-            let mut row = init_row;
-            if let Some(ScrolledColumn {
-                visible_cards,
-                at_edge,
-            }) = Self::scrolled_column(&column.0, self.selection_count(index))
-            {
-                if !matches!(at_edge, Some(CardColumnScroll::AtMaxRow)) {
-                    self.set_colors(White, Green);
-                    self.draw_text(col - 1, row, "↑  ↑");
-                }
-                if !matches!(at_edge, Some(CardColumnScroll::AtMinRow)) {
-                    self.set_colors(White, Green);
-                    self.draw_text(
-                        col - 1,
-                        row - 1 + (visible_cards.len() * Self::COLUMNS_ROW_STEP),
-                        "↓  ↓",
-                    );
-                }
-
-                for (card, card_state) in visible_cards {
-                    self.display_card(card, card_state, col, row);
-                    row += Self::COLUMNS_ROW_STEP;
-                }
-            } else {
-                for (card, card_state) in &column.0 {
-                    self.display_card(*card, *card_state, col, row);
-                    row += Self::COLUMNS_ROW_STEP;
-                }
-            }
-            col += Self::COLUMNS_COL_STEP;
-        }
     }
 
     const PILES_INIT_COL: usize = 48;
---
+++ src/draw/card_column.rs
@@ -0,0 +1,140 @@
+//! Draws a column of cards in the tableau.
+
+use super::Draw;
+use crate::cards::Card;
+use crate::game_state::{CardState, GameState};
+use crate::selection::Selection;
+
+enum CardColumnScroll {
+    AtMaxRow,
+    AtMinRow,
+}
+
+struct ScrolledColumn {
+    visible_cards: Vec<(Card, CardState)>,
+    at_edge: Option<CardColumnScroll>,
+}
+
+impl Draw {
+    pub(super) const COLUMNS_INIT_COL: usize = 8;
+    pub(super) const COLUMNS_INIT_ROW: usize = 2;
+    pub(super) const COLUMNS_COL_STEP: usize = 5;
+    pub(super) const COLUMNS_ROW_STEP: usize = 1;
+    pub(super) fn display_columns(&mut self, game_state: &GameState) {
+        use termion::color::*;
+        let (init_col, init_row) = (Self::COLUMNS_INIT_COL, Self::COLUMNS_INIT_ROW);
+        let mut col = init_col;
+        for (index, column) in game_state.columns.iter().enumerate() {
+            let mut row = init_row;
+            if let Some(ScrolledColumn {
+                visible_cards,
+                at_edge,
+            }) = Self::scrolled_column(&column.0, self.selection_count(index))
+            {
+                if !matches!(at_edge, Some(CardColumnScroll::AtMaxRow)) {
+                    self.set_colors(White, Green);
+                    self.draw_text(col - 1, row, "↑  ↑");
+                }
+                if !matches!(at_edge, Some(CardColumnScroll::AtMinRow)) {
+                    self.set_colors(White, Green);
+                    self.draw_text(
+                        col - 1,
+                        row - 1 + (visible_cards.len() * Self::COLUMNS_ROW_STEP),
+                        "↓  ↓",
+                    );
+                }
+
+                for (card, card_state) in visible_cards {
+                    self.display_card(card, card_state, col, row);
+                    row += Self::COLUMNS_ROW_STEP;
+                }
+            } else {
+                for (card, card_state) in &column.0 {
+                    self.display_card(*card, *card_state, col, row);
+                    row += Self::COLUMNS_ROW_STEP;
+                }
+            }
+            col += Self::COLUMNS_COL_STEP;
+        }
+    }
+
+    const COLUMN_MAX_VISIBLE_CARDS: usize = 7;
+    /// Scrolled offset in card column + position info, or None if not scrolled
+    fn scrolled_column_offset(
+        cards: usize,
+        selected: usize,
+    ) -> Option<(usize, Option<CardColumnScroll>)> {
+        use std::cmp::min;
+        if cards <= Self::COLUMN_MAX_VISIBLE_CARDS {
+            return None;
+        }
+
+        let range = cards - Self::COLUMN_MAX_VISIBLE_CARDS;
+        let a = cards - min(selected, cards);
+        let offset = min(a, range);
+
+        let position = match offset {
+            0 => Some(CardColumnScroll::AtMaxRow),
+            x if x == range => Some(CardColumnScroll::AtMinRow),
+            _ => None,
+        };
+
+        Some((offset, position))
+    }
+
+    /// A scrolled card column's visible cards, or None if not scrolled
+    fn scrolled_column(cards: &Vec<(Card, CardState)>, selected: usize) -> Option<ScrolledColumn> {
+        Self::scrolled_column_offset(cards.len(), selected).map(|(offset, at_edge)| {
+            ScrolledColumn {
+                visible_cards: cards[offset..offset + Self::COLUMN_MAX_VISIBLE_CARDS].into(),
+                at_edge,
+            }
+        })
+    }
+
+    /// A column's active selection count; 0 if not selected.
+    fn selection_count(&mut self, column_index: usize) -> usize {
+        if let Selection::Column { index, card_count } = self.cursor {
+            if column_index == index {
+                return card_count;
+            }
+        }
+        if let Some(Selection::Column { index, card_count }) = self.selected {
+            if column_index == index {
+                return card_count;
+            }
+        }
+        0
+    }
+
+    pub(super) fn draw_card_column_selection_cursor(
+        &mut self,
+        game_state: &GameState,
+        col: usize,
+        index: usize,
+        card_count: usize,
+    ) {
+        let length = game_state.columns[index].0.len();
+        let (scroll, _) = Self::scrolled_column_offset(length, card_count).unwrap_or((0, None));
+
+        let upper = Self::COLUMNS_INIT_ROW
+            + Self::COLUMNS_ROW_STEP
+                * length
+                    .checked_sub(scroll)
+                    .expect("column scroll should not exceed total cards");
+        let lower = upper
+            .checked_sub(card_count)
+            .expect("should not select nonexistent cards");
+
+        // Don't draw past the end of the column
+        let upper = std::cmp::min(
+            upper,
+            Self::COLUMNS_INIT_ROW + Self::COLUMNS_ROW_STEP * Self::COLUMN_MAX_VISIBLE_CARDS,
+        );
+
+        for row in lower..upper {
+            self.draw_text(col - 1, row, "[");
+            self.draw_text(col + 3, row, "]");
+        }
+    }
+}
